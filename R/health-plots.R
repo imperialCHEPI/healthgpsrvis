a8f737b726aebdc16bbf5dba323cbb3eb5bca5bc
Saranjeet Kaur <s.bhogal@imperial.ac.uk>
2024-07-17 15:50:32 +0100

    include tests for

--- R/health-plots.R
+++ R/health-plots.R
@@ -34,14 +34,14 @@ riskfactors <- function(riskft, data_mean_weighted) {
                   ggplot2::aes(x = data_mean_weighted$time,
                                y = get(y_value),
                                group = source)) +
-    ggplot2::geom_line(ggplot2::aes(col = source), size = 1) +
+    ggplot2::geom_line(ggplot2::aes(col = source), linewidth = 1) +
     ggplot2::ggtitle(toupper(riskft)) +
     ggplot2::xlab("Year") +
     ggplot2::ylab(y_label) +
     ggplot2::labs(fill = "Source") +
     ggplot2::scale_x_continuous(limits = c(2020, 2055), breaks = c(2020, 2025, 2030, 2035, 2040, 2045, 2050, 2055)) +
     ggplot2::scale_y_continuous(labels = scaleFUN) +
-    hgps_theme
+    hgps_theme()
 }
 
 #' Plot of Difference in Risk Factor
--- tests/testthat/test-health-plots.R
+++ tests/testthat/test-health-plots.R
@@ -1,3 +1,33 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("riskfactors function works correctly", {
+  # Create sample data
+  data_mean_weighted <- data.frame(
+    time = rep(seq(2020, 2055, by = 5), 3),
+    weighted_bmi = runif(24, 25, 38),
+    weighted_energyintake = runif(24, 1700, 2750),
+    weighted_fat = runif(24, 38, 120),
+    weighted_obesity = runif(24, 0.1, 0.7),
+    weighted_protein = runif(24, 46, 210),
+    weighted_sodium = runif(24, 874, 2768),
+    source = rep(c("Source_1", "Source_2", "Source_3"), each = 8)
+  )
+
+  # Test for valid input
+  plot <- riskfactors("bmi", data_mean_weighted)
+  expect_s3_class(plot, "ggplot")
+  expect_equal(plot$labels$title, "BMI")
+  expect_equal(plot$labels$y, "BMI (weighted)")
+
+  plot <- riskfactors("ei", data_mean_weighted)
+  expect_s3_class(plot, "ggplot")
+  expect_equal(plot$labels$title, "EI")
+  expect_equal(plot$labels$y, "Energy intake (weighted)")
+
+  plot <- riskfactors("sodium", data_mean_weighted)
+  expect_s3_class(plot, "ggplot")
+  expect_equal(plot$labels$title, "SODIUM")
+  expect_equal(plot$labels$y, "Sodium (weighted)")
+
+  # Test for invalid input
+  expect_error(riskfactors("invalid_riskft", data_mean_weighted),
+               "Invalid risk factor. Choose from: 'bmi', 'ei', 'fat', 'obese', 'protein', 'sodium'.")
 })
