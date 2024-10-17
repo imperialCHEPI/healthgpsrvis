6b666dc4787313d79b88b189a68d306299330257
Saranjeet Kaur <s.bhogal@imperial.ac.uk>
2024-10-17 20:46:52 +0100

    initialise testing for data-process.R

---
+++ tests/testthat/test-data-process.R
@@ -0,0 +1,8 @@
+# Testing gen_data_weighted_rf() function
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+# Testing gen_data_weighted_ds() function
+
+# Testing gen_data_weighted_burden() function
