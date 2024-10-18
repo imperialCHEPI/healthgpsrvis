58a164494c56ffc341e70d7fe5d130ff9a96d027
Saranjeet Kaur <s.bhogal@imperial.ac.uk>
2024-10-18 17:57:44 +0100

    :memo: include data_ps3_reformulation for unit tests

--- tests/testthat/test-data-process.R
+++ tests/testthat/test-data-process.R
@@ -1,3 +1,16 @@
+# Testing .rds file
+test_that("rds file is not empty", {
+  # Get the path to the .rds file
+  filepath <- testthat::test_path("testdata", "data_ps3_reformulation")
+
+  # Read the .rds file
+  data <- readRDS(filepath)
+
+  # Check if the .rds file is not empty
+  expect_gt(length(data), 0)
+})
+
+
 # Testing gen_data_weighted_rf() function
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
