1ef4d53a1951a6f533e4421a5370451630d0d30c
Saranjeet Kaur <s.bhogal@imperial.ac.uk>
2025-02-21 09:13:26 +0000

    update config and tests

--- tests/testthat/test-data-process.R
+++ tests/testthat/test-data-process.R
@@ -199,32 +199,31 @@ test_that("Columns in the generated dataframe", {
 })
 
 # Testing gen_data_weighted_bd_spline() function
-# test_that("Columns in the generated dataframe", {
-#   # Get the path to the .rds file
-#   filepath <- testthat::test_path("testdata", "data_ps3_reformulation")
-#
-#   # Read the .rds file
-#   data <- readRDS(filepath)
-#
-#   # Generate the weighted data
-#   data_weighted <- gen_data_weighted(data, configname = "default")
-#
-#   # Generate the weighted data for the risk factors (using 'bd' instead of
-#   # 'burden' to keep lintr happy)
-#   data_weighted_bd_wide_collapse <- gen_data_weighted_burden(data_weighted,
-#                                                              configname = "default")
-#
-#   # Generate a data frame with spline smoothing applied for burden of disease
-#   data_weighted_burden_spline <- gen_data_weighted_bd_spline(
-#     data_weighted_bd_wide_collapse
-#   )
-#
-#   # Check if the data has the expected number of columns
-#   expect_equal(ncol(data_weighted_burden_spline), 4)
-#
-#   # Check if the data has the expected column names
-#   expect_equal(
-#     colnames(data_weighted_burden_spline),
-#     c("time", "cumdiff_daly_mean", "cumdiff_daly_ci_low", "cumdiff_daly_ci_high")
-#   )
-# })
+test_that("Columns in the generated dataframe", {
+  # Get the path to the .rds file
+  filepath <- testthat::test_path("testdata", "data_ps3_reformulation")
+
+  # Read the .rds file
+  data <- readRDS(filepath)
+
+  # Generate the weighted data
+  data_weighted <- gen_data_weighted(data, configname = "default")
+
+  # Generate the weighted data for the risk factors (using 'bd' instead of
+  # 'burden' to keep lintr happy)
+  data_weighted_bd_wide_collapse <- gen_data_weighted_burden(data_weighted, configname = "default")
+
+  # Generate a data frame with spline smoothing applied for burden of disease
+  data_weighted_burden_spline <- gen_data_weighted_bd_spline(
+    data_weighted_bd_wide_collapse
+  )
+
+  # Check if the data has the expected number of columns
+  expect_equal(ncol(data_weighted_burden_spline), 4)
+
+  # Check if the data has the expected column names
+  expect_equal(
+    colnames(data_weighted_burden_spline),
+    c("time", "cumdiff_daly_mean", "cumdiff_daly_ci_low", "cumdiff_daly_ci_high")
+  )
+})
